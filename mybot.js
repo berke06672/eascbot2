7c8fe9781dbcfb6be5abda27dd776002fcb79155
berke06672 <43908643+berke06672@users.noreply.github.com>
2018-10-11 18:33:00 +0300

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -218,6 +218,35 @@ const embed = new Discord.RichEmbed()
   message.channel.send({embed});
 
   } else
+	  
+	  	  if (message.content.startsWith(prefix + "c!estoniahuman")) {
+const embed = new Discord.RichEmbed()
+  .setTitle("📁 estoniahuman")
+  .setAuthor("About estoniahuman", "https://media.discordapp.net/attachments/488307727005646858/496286658958721034/express_urself_final11_-_Kopya_2.jpg?width=295&height=300")
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor(0x01AE86)
+  .setDescription("Floor Manager of Earthvision Song Contest")
+  .setFooter("by EASC", "https://media.discordapp.net/attachments/488307727005646858/496286658958721034/express_urself_final11_-_Kopya_2.jpg?width=295&height=300")
+  .setImage("https://media.discordapp.net/attachments/488307727005646858/496286658958721034/express_urself_final11_-_Kopya_2.jpg?width=295&height=300")
+  .setThumbnail("https://media.discordapp.net/attachments/488307727005646858/496286658958721034/express_urself_final11_-_Kopya_2.jpg?width=295&height=300")
+  /*
+   * Takes a Date object, defaults to current date.
+   */
+  .setTimestamp()
+  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+  .addField(":bust_in_silhouette: About estoniahuman","?, :flag_ee: Estonian(*Do you have any doubt?*)")
+  .addField("**EASC History**","First appearance :zero::one:")
+  .addField("**Last appearance:** :zero::one:","**Apperances:** :one:")
+  .addField("**Best Result:** :zero::one:","**Worst Result:** Na/N")
+  .addField(":zero::one: : Urban Symphony - Rändajad ","**Points:** 0 **Points:** 0th ")
+				   
+
+
+  message.channel.send({embed});
+
+  } else
 	if (message.content.startsWith(prefix + "bot")) {
     message.channel.send("thanks for helping me");
   }  else
