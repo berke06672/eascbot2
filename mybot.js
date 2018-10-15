d08118c3b11ef7c619d2d6985ebe4939986e5455
berke06672 <43908643+berke06672@users.noreply.github.com>
2018-10-15 18:52:41 +0300

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -326,11 +326,10 @@ if (message.content.startsWith(prefix + "hq")) {
 } else
 	
 	if (message.content.startsWith(prefix + "infobot")) {
-  let bicon = bot.user.displayAvatarURL;
+
     let botembed = new Discord.RichEmbed()
     .setDescription("Bot Information")
     .setColor("#15f153")
-    .setThumbnail(bicon)
     .addField("Bot Name", bot.user.username)
     .addField("Created On", bot.user.createdAt);
 
