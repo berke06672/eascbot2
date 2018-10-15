420ecb6a51d947b9f44fcf40273d50695d1633e5
berke06672 <43908643+berke06672@users.noreply.github.com>
2018-10-15 18:48:00 +0300

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -325,8 +325,16 @@ if (message.content.startsWith(prefix + "hq")) {
 
 } else
 	
-	if (message.content.startsWith(prefix + "kate")) {
-  message.channel.send("I believe in :kate-1:");
+	if (message.content.startsWith(prefix + "infobot")) {
+  let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+    .setDescription("Bot Information")
+    .setColor("#15f153")
+    .setThumbnail(bicon)
+    .addField("Bot Name", bot.user.username)
+    .addField("Created On", bot.user.createdAt);
+
+    message.channel.send(botembed);
 
 } else
 	if (message.content.startsWith(prefix + "can i count on you")) {
